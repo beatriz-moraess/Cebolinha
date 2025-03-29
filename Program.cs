142e449f17f4135d42c0baf8a63c8a2b3555ab7d
Beatriz <beatriz.giovanna@gmail.com>
2025-03-29 20:14:50 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+
+Console.WriteLine("Escreva uma frase para o Cebolinha falar: ");
+
+string frase = Console.ReadLine()!;
+string cebolinha = frase.Replace("R","L").Replace("r","l");
+
+Console.WriteLine($"Cebolinha: {cebolinha}");
+
